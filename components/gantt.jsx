853d7c45a1b137292f663b20f841611a63a166be
zakuni <kunio038@gmail.com>
2015-11-03 23:22:56 +0900

    use d3 for gantt

--- components/gantt.jsx
+++ components/gantt.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import moment from 'moment'
+import d3 from 'd3'
 
 class Gantt extends React.Component {
+  componentDidMount() {
+    var tasks = d3.select(React.findDOMNode(this)).append('g')
+      .attr('class', 'tasks');
+
+    var task = tasks.selectAll(".task")
+      .data(this.props.events);
+
+    task.enter().append('g').attr('class', 'task')
+      .call( (selection) => {
+        selection.append('text')
+          .text(function(event){ return event.summary })
+          .attr("x", 20)
+          .attr("y", function(d, i){ return (i*40)+42 });
+
+        selection.append('text')
+          .text(function(event){ return event.start.dateTime })
+          .attr("x", 520)
+          .attr("y", function(d, i){ return (i*40)+42 });
+
+        selection.append('text')
+          .text(function(event){ return event.end.dateTime })
+          .attr("x", "720")
+          .attr("y", function(d, i){ return (i*40)+42 });
+
+        selection.append('text')
+          .text(function(event){ return moment(event.end.dateTime).diff(moment(event.start.dateTime), "minutes")})
+          .attr("x", "920")
+          .attr("y", function(d, i){ return (i*40)+42 });
+      });
+  }
   render() {
-    var eventslist = this.props.events.map((event, i) =>
-      <g key={event.id}>
-        <text x="20" y={(i*40)+42}>{event.summary}</text>
-        <text x="520" y={(i*40)+42}>{event.start.dateTime}</text>
-        <text x="720" y={(i*40)+42}>{event.end.dateTime}</text>
-        <text x="920" y={(i*40)+42}>{moment(event.end.dateTime).diff(moment(event.start.dateTime), "minutes")}</text>
-      </g>
-    );
     return (
-      <svg className="uk-height-1-1" width="100%">
-        {eventslist}
-      </svg>
+      <svg className="uk-height-1-1" width="100%"></svg>
     );
   }
 }
